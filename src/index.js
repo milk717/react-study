1feb96cba5523bc4bfc18c6d767269c161c2827b
milk717 <robolindasoo@gmail.com>
2022-07-05 10:31:13 +0900

    Clock 컴포넌트 함수

--- src/index.js
+++ src/index.js
@@ -6,60 +6,23 @@ import App from "./App";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
-function formatDate(date) {
-    return date.toLocaleDateString();
-}
-
-const comment = {
-    date: new Date(),
-    text: 'I hope you enjoy learning React!',
-    author: {
-        name: 'Hello Kitty',
-        avatarUrl: 'http://placekitten.com/g/64/64'
-    }
-};
-
-function Avatar(props){
+function Clock(props) {
     return (
-        <img className="Avatar"
-             src={props.user.avatarUrl}
-             alt={props.user.name}
-        />
-    );
-}
-
-function UserInfo(props){
-    return (
-        <div className="UserInfo">
-            <Avatar user = {props.author}/>
-            <div className="UserInfo-name">
-                {props.author.name}
-            </div>
+        <div>
+            <h1>Hello, World!</h1>
+            <h2>It is {props.date.toLocaleTimeString()}.</h2>
         </div>
     );
 }
 
-function Comment(props) {
-    return (
-        <div className="Comment">
-            <UserInfo author = {props.author}/>
-            <div className="Comment-text">
-                {props.text}
-            </div>
-            <div className="Comment-date">
-                {formatDate(props.date)}
-            </div>
-        </div>
+function tick() {
+    root.render(
+        <Clock date={new Date()}/>
     );
 }
 
-root.render(
-    <Comment
-        date ={comment.date}
-        text ={comment.text}
-        author = {comment.author}
-    />
-);
+setInterval(tick, 1000);
+
 
 //앱 퍼포먼스를 로그로 보여줌
 reportWebVitals(console.log);
