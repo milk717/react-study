eaed03619760f1e69787bc52188ad7a5f8ba7efb
milk717 <robolindasoo@gmail.com>
2022-07-05 10:00:54 +0900

    1초마다 ReactDOM.render() 호출

--- src/index.js
+++ src/index.js
@@ -5,8 +5,18 @@ import reportWebVitals from './reportWebVitals';
 import App from "./App";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-    <App/>
-);
 
-reportWebVitals();
+function tick(){
+    const element = (
+      <div>
+          <h1>Hello, world</h1>
+          <h2>It is {new Date().toLocaleTimeString()}.</h2>
+      </div>
+    );
+    root.render(element);
+}
+
+setInterval(tick, 1000);
+
+//앱 퍼포먼스를 로그로 보여줌
+reportWebVitals(console.log);
