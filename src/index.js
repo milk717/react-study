ecb9ab88930080ff9eafcbd834ddeafb30eeb2ca
milk717 <robolindasoo@gmail.com>
2022-07-05 17:36:38 +0900

    Hook: useState_버튼을 클릭하면 숫자 증가

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
@@ -6,72 +6,21 @@ import App from "./App";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
-const dataId = 2;
+function Example(){
+    const [count, setCount] = useState(0);
 
-function ListItem(props) {
-    return <li>{props.value}</li>;
-}
-
-function TodoList(props) {
-    const listValues = props.values;
     return(
-        <ul>
-            {listValues.map((value) =>
-                    <ListItem key={value.id} value={value.content}/>)
-            }
-        </ul>
+        <div>
+            <p>You clicked {count} times</p>
+            <button onClick={()=>setCount(count + 1)}>
+                Click me
+            </button>
+        </div>
     );
 }
 
-class TodoListForm extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            value: "",
-            todolist: [
-                {
-                    id: 1,
-                    content: '첫 번째 항목'
-                }
-            ]
-        }
-
-        this.handleChange = this.handleChange.bind(this);
-        this.handleSubmit = this.handleSubmit.bind(this);
-    }
-
-    handleChange(event) {
-        this.setState({value: event.target.value});
-        //console.log(this.state.value);
-    }
-
-    handleSubmit(event) {
-        const {todolist} = this.state;
-        this.setState({
-            todolist: todolist.concat({id: dataId, content: this.state.value}),
-            value: ""
-        });
-        dataId++;
-    }
-
-
-    render() {
-        return (
-            <div>
-                <form onSubmit={this.handleSubmit}>
-                    <input type="text" value={this.state.value} onChange={this.handleChange}/>
-                    <input type="submit" value="추가"/>
-                </form>
-                <TodoList values={this.state.todolist}/>
-            </div>
-        );
-    }
-}
-
 root.render(
-    <div>
-        <TodoListForm/>
-    </div>
+    <Example/>
 );
 
 //앱 퍼포먼스를 로그로 보여줌
