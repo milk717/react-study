9f0a0de137c5f3238f5eeef004b7189d80312959
milk717 <robolindasoo@gmail.com>
2022-07-05 17:03:22 +0900

    폼 입력받아서 항목 추가하기

--- src/index.js
+++ src/index.js
@@ -6,32 +6,73 @@ import App from "./App";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
+const dataId = 2;
+
+function ListItem(props) {
+    return <li>{props.value}</li>;
+}
+
+function TodoList(props) {
+    const listValues = props.values;
+    return(
+        <ul>
+            {listValues.map((value) =>
+                    <ListItem key={value.id} value={value.content}/>)
+            }
+        </ul>
+    );
+}
+
 class TodoListForm extends React.Component {
     constructor(props) {
         super(props);
-        this.state = {value: ""}
+        this.state = {
+            value: "",
+            todolist: [
+                {
+                    id: 1,
+                    content: '첫 번째 항목'
+                }
+            ]
+        }
 
         this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
     }
 
     handleChange(event) {
         this.setState({value: event.target.value});
-        console.log(this.state.value);
+        //console.log(this.state.value);
     }
+
+    handleSubmit(event) {
+        const {todolist} = this.state;
+        this.setState({
+            todolist: todolist.concat({id: dataId, content: this.state.value}),
+            value: ""
+        });
+        dataId++;
+    }
+
 
     render() {
         return (
-            <form>
-                <input type = "text" value = {this.state.value} onChange={this.handleChange}/>
-            </form>
+            <div>
+                <form onSubmit={this.handleSubmit}>
+                    <input type="text" value={this.state.value} onChange={this.handleChange}/>
+                    <input type="submit" value="추가"/>
+                </form>
+                <TodoList values={this.state.todolist}/>
+            </div>
         );
     }
 }
 
-
 root.render(
-    <TodoListForm/>
+    <div>
+        <TodoListForm/>
+    </div>
 );
 
 //앱 퍼포먼스를 로그로 보여줌
-reportWebVitals(console.log);
+// reportWebVitals(console.log);
