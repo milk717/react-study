33c8d5be33af3a0de994cbfbb0fb12f969b142ad
milk717 <robolindasoo@gmail.com>
2022-07-05 14:24:13 +0900

    조건부 렌더링 (로그인 로그아웃)

--- src/index.js
+++ src/index.js
@@ -14,6 +14,22 @@ function GuestGreeting(props) {
     return <h1>Please sign up.</h1>
 }
 
+function LoginButton(props) {
+    return (
+        <button onClick={props.onClick}>
+            Login
+        </button>
+    );
+}
+
+function LogoutButton(props) {
+    return (
+        <button onClick={props.onClick}>
+            Logout
+        </button>
+    );
+}
+
 function Greeting(props) {
     const isLoggedIn = props.isLoggedIn;
     if (isLoggedIn) {
@@ -23,8 +39,40 @@ function Greeting(props) {
     }
 }
 
+class LoginControl extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {isLoggedIn: false};
+    }
+
+    handleLoginClick = () => {
+        this.setState({isLoggedIn: true});
+    }
+
+    handleLogoutClick = () =>{
+        this.setState({isLoggedIn: false});
+    }
+
+    render() {
+        const isLoggedIn = this.state.isLoggedIn;
+        let button;
+        if (isLoggedIn) {
+            button = <LogoutButton onClick={this.handleLogoutClick}/>;
+        }else{
+            button = <LoginButton onClick={this.handleLoginClick}/>;
+        }
+
+        return (
+            <div>
+                <Greeting isLoggedIn = {isLoggedIn}/>
+                {button}
+            </div>
+        );
+    }
+}
+
 root.render(
-    <Greeting isLoggedIn={false}/>
+    <LoginControl/>
 );
 
 //앱 퍼포먼스를 로그로 보여줌
