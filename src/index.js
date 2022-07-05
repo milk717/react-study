25507dddf49181a67e1d38c12fef3d15055e55ad
milk717 <robolindasoo@gmail.com>
2022-07-05 10:37:19 +0900

    Clock 클래스로 변경

--- src/index.js
+++ src/index.js
@@ -6,23 +6,25 @@ import App from "./App";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
-function Clock(props) {
-    return (
-        <div>
-            <h1>Hello, World!</h1>
-            <h2>It is {props.date.toLocaleTimeString()}.</h2>
-        </div>
-    );
+class Clock extends React.Component{
+    constructor(props) {
+        super(props);
+        this.state = {date: new Date()};
+    }
+
+    render() {
+        return(
+            <div>
+                <h1>Hello, World!</h1>
+                <h2>It is {this.state.date.toLocaleTimeString()}.</h2>
+            </div>
+        );
+    }
 }
 
-function tick() {
-    root.render(
-        <Clock date={new Date()}/>
-    );
-}
-
-setInterval(tick, 1000);
-
+root.render(
+    <Clock/>
+);
 
 //앱 퍼포먼스를 로그로 보여줌
 reportWebVitals(console.log);
