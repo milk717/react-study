7ff1a792f75412f883027ee02b030840ee5dd92b
milk717 <robolindasoo@gmail.com>
2022-07-05 11:17:55 +0900

    event 처리

--- src/index.js
+++ src/index.js
@@ -6,44 +6,21 @@ import App from "./App";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
-class Clock extends React.Component{
-    constructor(props) {
-        super(props);
-        this.state = {date: new Date()};
+function Form(){
+    function handleSubmit(e) {
+        e.preventDefault();
+        console.log('You clicked submit.');
     }
 
-    //컴포넌트가 DOM에 렌더링 될 때마다 실행됨
-    componentDidMount() {
-        this.timerId = setInterval(
-            ()=> this.tick(),
-            1000
-        );
-    }
-
-    //컴포넌트에 의해 생성된 DOM이 삭제될 때마다 실행됨
-    componentWllUnmount() {
-        //타이머의 인터벌 삭제
-        clearInterval(this.timerId);
-    }
-
-    tick(){
-        this.setState({
-            date: new Date(),
-        });
-    }
-
-    render() {
-        return(
-            <div>
-                <h1>Hello, World!</h1>
-                <h2>It is {this.state.date.toLocaleTimeString()}.</h2>
-            </div>
-        );
-    }
+    return(
+        <form onSubmit={handleSubmit}>
+            <button type="submit">Submit</button>
+        </form>
+    );
 }
 
 root.render(
-    <Clock/>
+    <Form/>
 );
 
 //앱 퍼포먼스를 로그로 보여줌
