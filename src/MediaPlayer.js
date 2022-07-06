8f6094e61f99da31bb209c245d211b45eae4fb87
milk717 <robolindasoo@gmail.com>
2022-07-06 17:22:28 +0900

    react-player custom: 재생, 정지 로직만 구현

--- src/MediaPlayer.js
+++ src/MediaPlayer.js
@@ -3,12 +3,12 @@ import Container from '@mui/material/Container';
 import styled, { css } from 'styled-components';
 import Typography from '@mui/material/Typography'
 import Icon from "@mui/material/Icon"
-import React from "react";
-import IconButton from '@mui/material/IconButton';
+import React, { useState, useEffect } from "react";
 import Forward10Icon from '@mui/icons-material/Forward10';
 import PlayCircleIcon from '@mui/icons-material/PlayCircle';
 import Replay10Icon from '@mui/icons-material/Replay10';
-
+import FeaturedVideoIcon from '@mui/icons-material/FeaturedVideo';
+import PauseCircleIcon from '@mui/icons-material/PauseCircle';
 
 const ControlsWrapper = styled.div`
     position: absolute;
@@ -17,7 +17,7 @@ const ControlsWrapper = styled.div`
     right: 0;
     bottom: 0;
     background: rgba(0,0,0,6);
-    opacity:0.6;
+    opacity:0.5;
     display: flex;
     flex-direction: column;
     justify-content: center;
@@ -53,8 +53,28 @@ const ControlsIcons = styled.button`
     };
 `;
 
-
 const MediaPlayer = ({playList, index}) => {
+    const [state, setState] = useState({
+        playing: true,     // 재생중인지
+        muted: false,      // 음소거인지
+        volume: 0.5,       // 볼륨크기
+        playbackRate: 1.0, // 배속
+        played: 0,         // 재생의 정도 (value)
+        seeking: false,    // 재생바를 움직이고 있는지
+        duration: 0,       // 전체 시간
+        pip: true
+    });
+
+    const handlePlayPauseButtonClick = () =>{
+        setState({
+            ...state, playing: !state.playing
+        });
+    };
+
+    const handlePipButtonClick = () => {
+
+    };
+
     return (
         <Container>
             <h2>동영상 재생 페이지 입니다.</h2>
@@ -63,103 +83,36 @@ const MediaPlayer = ({playList, index}) => {
                     url={'https://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4'}    // 플레이어 url
                     width='100%'         // 플레이어 크기 (가로)
                     height='100%'        // 플레이어 크기 (세로)
-                    playing={true}        // 자동 재생 on
+                    playing={state.playing}        // 자동 재생 on
                     muted={true}          // 자동 재생 on (디폴트 음소거일 때만 자동재생됨)
-                    controls={false}       // 플레이어 컨트롤 노출 여부
+                    controls={true}       // 플레이어 컨트롤 노출 여부
                     light={false}         // 플레이어 모드
                     pip={true}            // pip 모드 설정 여부
                     poster={'https://commondatastorage.googleapis.com/gtv-videos-bucket/sample/images/BigBuckBunny.jpg'}   // 플레이어 초기 포스터 사진
                     // onEnded={() => handleVideo()}  // 플레이어 끝났을 때 이벤트
                 />
+                {/*중간 버튼*/}
                 <ControlsWrapper>
                     <MiddleControlsWrapper >
                         <ControlsIcons>
                             <Replay10Icon fontSize="inherit" />
                         </ControlsIcons>
 
-                        <ControlsIcons>
+                        <ControlsIcons  onClick={handlePlayPauseButtonClick} >
                             <PlayCircleIcon fontSize="inherit" />
                         </ControlsIcons>
 
                         <ControlsIcons>
                             <Forward10Icon fontSize="inherit" />
                         </ControlsIcons>
+
+                        {/*이부분은 아래쪽으로 옮길거임*/}
+                        <ControlsIcons>
+                            <FeaturedVideoIcon fontSize="inherit" />
+                        </ControlsIcons>
                     </MiddleControlsWrapper>
 
-                    {/*/!* bottom controls *!/*/}
-                    {/*<Grid*/}
-                    {/*    container*/}
-                    {/*    direction="row"*/}
-                    {/*    justify="space-between"*/}
-                    {/*    alignItems="center"*/}
-                    {/*    style={{ padding: 16 }}*/}
-                    {/*>*/}
-                    {/*    <Grid item xs={12}>*/}
-                    {/*        <PrettoSlider*/}
-                    {/*            min={0}*/}
-                    {/*            max={100}*/}
-                    {/*            defaultValue={20}*/}
-                    {/*            ValueLabelComponent={ValueLabelComponent}*/}
-                    {/*        />*/}
-                    {/*    </Grid>*/}
-
-                    {/*    <Grid item>*/}
-                    {/*        <Grid container alignItems="center" direction="row">*/}
-                    {/*            <IconButton className={classes.bottomIcons}>*/}
-                    {/*                <PlayArrowIcon fontSize="large" />*/}
-                    {/*            </IconButton>*/}
-
-                    {/*            <IconButton className={classes.bottomIcons}>*/}
-                    {/*                <VolumeUpIcon fontSize="large" />*/}
-                    {/*            </IconButton>*/}
-
-                    {/*            <Slider*/}
-                    {/*                min={0}*/}
-                    {/*                max={100}*/}
-                    {/*                defaultValue={100}*/}
-                    {/*                className={classes.volumeSlider}*/}
-                    {/*            />*/}
-
-                    {/*            <Button variant="text" style={{ color: "#fff", marginLeft: 16 }}>*/}
-                    {/*                <Typography>05:05</Typography>*/}
-                    {/*            </Button>*/}
-                    {/*        </Grid>*/}
-                    {/*    </Grid>*/}
-                    {/*    <Grid item>*/}
-                    {/*        <Button*/}
-                    {/*            onClick={handlePopover}*/}
-                    {/*            variant="text"*/}
-                    {/*            className={classes.bottomIcons}*/}
-                    {/*        >*/}
-                    {/*            <Typography>1X</Typography>*/}
-                    {/*        </Button>*/}
-                    {/*        <Popover*/}
-                    {/*            id={id}*/}
-                    {/*            open={open}*/}
-                    {/*            anchorEl={anchorEl}*/}
-                    {/*            onClose={handleClose}*/}
-                    {/*            anchorOrigin={{*/}
-                    {/*                vertical: "top",*/}
-                    {/*                horizontal: "center",*/}
-                    {/*            }}*/}
-                    {/*            transformOrigin={{*/}
-                    {/*                vertical: "bottom",*/}
-                    {/*                horizontal: "center",*/}
-                    {/*            }}*/}
-                    {/*        >*/}
-                    {/*            <Grid container direction="column-reverse">*/}
-                    {/*                {[0.5, 1, 1.5, 2].map((rate) => (*/}
-                    {/*                    <Button variant="text">*/}
-                    {/*                        <Typography color="secondary">{rate}</Typography>*/}
-                    {/*                    </Button>*/}
-                    {/*                ))}*/}
-                    {/*            </Grid>*/}
-                    {/*        </Popover>*/}
-                    {/*        <IconButton className={classes.bottomIcons}>*/}
-                    {/*            <FullScreenIcon fontSize="large" />*/}
-                    {/*        </IconButton>*/}
-                    {/*    </Grid>*/}
-                    {/*</Grid>*/}
+                    {/*아래쪽 버튼*/}
                 </ControlsWrapper>
             </PlayerWapper>
 
